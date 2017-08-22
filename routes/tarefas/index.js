e435c857b6280964a905fb1dd28f559cbf34fc90
SeatSpnos <segmento.especializado@gmail.com>
2017-08-22 17:12:44 +0100

    Add files via upload

--- routes/tarefas/index.js
+++ routes/tarefas/index.js
@@ -4,8 +4,7 @@ const controller = require('../../controllers').tarefas;
 
 router.get('/all', controller.find.allTasks);
 router.post('/verify', controller.find.verify);
-router.get('/', controller.find.allByDate);
-router.get('/:date', controller.find.allByDate);
+router.get('/:date?', controller.find.allByDate);
 router.post('/', controller.insert);
 
 module.exports = router;
